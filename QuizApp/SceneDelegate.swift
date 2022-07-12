bd9f9a5e0d76d20b52f0920f9f2814835c300bdb
Ivan Kulundzic <ivan.kulundzic@five.agency>
2022-07-12 14:24:26 +0200

    Reverted the spacing/indentation to 4/4.

--- QuizApp/SceneDelegate.swift
+++ QuizApp/SceneDelegate.swift
@@ -8,32 +8,32 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-  
-  var window: UIWindow?
-  
-  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    guard let scene = (scene as? UIWindowScene) else { return }
-    setupInitialScene(with: scene)
-  }
-  
-  func sceneDidDisconnect(_ scene: UIScene) {}
-  
-  func sceneDidBecomeActive(_ scene: UIScene) {}
-  
-  func sceneWillResignActive(_ scene: UIScene) {}
-  
-  func sceneWillEnterForeground(_ scene: UIScene) {}
-  
-  func sceneDidEnterBackground(_ scene: UIScene) {}
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        setupInitialScene(with: scene)
+    }
+    
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {}
 }
 
 // MARK: - Private methods
 private extension SceneDelegate {
-  func setupInitialScene(with scene: UIWindowScene) {
-    let window = UIWindow(windowScene: scene)
-    self.window = window
-    let initialViewController = ViewController()
-    window.rootViewController = initialViewController
-    window.makeKeyAndVisible()
-  }
+    func setupInitialScene(with scene: UIWindowScene) {
+        let window = UIWindow(windowScene: scene)
+        self.window = window
+        let initialViewController = ViewController()
+        window.rootViewController = initialViewController
+        window.makeKeyAndVisible()
+    }
 }
