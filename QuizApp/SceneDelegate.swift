9501ea28555a09f308b55fa918f02add949c3655
Ivan Kulundžić <44921974+IvanKulundzic@users.noreply.github.com>
2022-07-12 15:32:29 +0200

    SwiftLint integration (#2)
    * Added SwiftLint to the project. Fixed some SwiftLint warnings.
    
    * Adjusted the methods code style.
    
    Co-authored-by: Ivan Kulundzic <ivan.kulundzic@five.agency>

--- QuizApp/SceneDelegate.swift
+++ QuizApp/SceneDelegate.swift
@@ -11,11 +11,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
         guard let scene = (scene as? UIWindowScene) else { return }
         setupInitialScene(with: scene)
     }
-    
+
     func sceneDidDisconnect(_ scene: UIScene) {}
 
     func sceneDidBecomeActive(_ scene: UIScene) {}
